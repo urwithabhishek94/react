73a858437d70f32d07214ea79b29bf3bf187d2fb
urwithabhishek94 <urwithabhishek94@gmail.com>
2020-07-30 14:16:52 +0530

    Updates

---
+++ precache-manifest.2af685348e53c1876521227f48e4668e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f1c5d28291bf2407df7ba1dfbb166f",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "f03ca568d752415128df",
+    "url": "/react/static/js/2.f0162ff6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react/static/js/2.f0162ff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a5683b44ce251e0216",
+    "url": "/react/static/js/main.5f2dcf39.chunk.js"
+  },
+  {
+    "revision": "5662f1b3f3dcfe5b621f",
+    "url": "/react/static/js/runtime-main.7bab3dbc.js"
+  }
+]);
